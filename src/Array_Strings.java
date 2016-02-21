ad96b6e024f6ad09a4a2bd228323d9c456db1f55
Ajay Malalikar <ajay.malalikar@gmail.com>
2016-02-20 22:59:51 -0800

    Netbeans commit

---
+++ src/Array_Strings.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author Ajay
+ */
+public class Array_Strings {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Functions
+{
+    //https://leetcode.com/problems/increasing-triplet-subsequence/
+    //Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.
+    public boolean increasingTriplet(int[] nums) {
+        if(nums.length > 0)
+        {
+            int min = nums[0];
+            int smin = Integer.MAX_VALUE;
+
+            for(int i=1; i<nums.length; i++)
+            {
+                if(nums[i]<=min)
+                    min = nums[i];
+                else if(nums[i] <= smin)
+                    smin = nums[i];
+                else if(nums[i] > smin && nums[i] > min)
+                    return true;
+            }
+            return false;
+        }
+        else
+            return false;
+    }
+
+    //https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+    //Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        int j=1;
+        while(i<nums.length && j<nums.length)
+        {
+            while(j<nums.length && nums[j] == nums[i])
+                j++;
+            if(j<nums.length)
+                nums[++i] = nums[j++];
+        }
+        return i+1;
+    }
+    
+    //https://leetcode.com/problems/longest-consecutive-sequence/
+    //https://leetcode.com/problems/longest-consecutive-sequence/
+    public int longestConsecutive(int[] a) {
+        HashSet<Integer> h = new HashSet();
+        for(int n: a)
+        {
+            h.add(n);
+        }
+        
+        int res = 1;
+        for(int i=0; i<a.length; i++)
+        {
+            if(!h.contains(a[i]-1))
+            {
+                int j = a[i];
+                while(h.contains(j))
+                    j++;
+                if(res < j-a[i])
+                    res = j-a[i];
+            }
+        }
+        return res;
+    }
+}
